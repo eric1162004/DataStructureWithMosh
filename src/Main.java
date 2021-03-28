3104c55be7c98bacf6696adc19f14147139bc49a
Eric Cheung <000437017@student.vcc.ca>
2021-03-28 13:37:49 -0700

    testing main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Map<Integer, String> map  =new HashMap<>();
+        map.put(1, "Mosh");
+        map.put(2, "John");
+        map.put(3, "Mary");
+        System.out.println(map);
+
     }
 }
